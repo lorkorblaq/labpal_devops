9ab42773843e0d4dee7e085479e1b569568863c5
lorkorblaq <f3miolok@gmail.com>
2024-05-30 10:51:01 +0100

    update

---
+++ docker/buildPush.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop all running containers
+docker stop $(docker ps -a -q)
+
+# Remove all containers
+docker rm $(docker ps -a -q)
+
+# Remove all images
+docker rmi $(docker images -a -q)
+
+# Build and push the first image
+docker build -t lorkorblaq/clinicalx_api:latest -f ../../clinicalx_api/Dockerfile ../../clinicalx_api
+docker push lorkorblaq/clinicalx_api:latest
+
+# Build and push the second image
+docker build -t lorkorblaq/clinicalx_api:latest -f ../../clinicalx_main/Dockerfile ../../clinicalx_main
+docker push lorkorblaq/clinicalx_main:latest
