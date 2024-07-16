f264e1b8216e29395f7e303e54a83a13296d26a6
lorkorblaq <f3miolok@gmail.com>
2024-07-16 18:02:58 +0100

    update

--- docker/buildPush.sh
+++ docker/buildPush.sh
@@ -16,8 +16,8 @@
 # docker build -t lorkorblaq/labpal_api:latest -f ../../clinicalx_api/Dockerfile ../../clinicalx_api
 # docker push lorkorblaq/labpal_api:latest
 
-# docker build -t lorkorblaq/labpal_nginx:secure -f ../../clinicalx_devops/nginx/Dockerfile ../../clinicalx_devops/nginx/
-# docker push lorkorblaq/labpal_nginx:secure
+docker build -t lorkorblaq/labpal_nginx:secure -f ../../clinicalx_devops/nginx/Dockerfile ../../clinicalx_devops/nginx/
+docker push lorkorblaq/labpal_nginx:secure
 
 
 # Build and push the seconds image
@@ -32,5 +32,5 @@
 # docker build -t lorkorblaq/gpt_engine:latest -f ../../labpal_gpt/gptenv/Dockerfile ../../labpal_gpt/gptenv
 # docker push lorkorblaq/gpt_engine:latest
 
-docker build -t lorkorblaq/labpal_gpt:latest -f ../../labpal_gpt/Dockerfile ../../labpal_gpt
-docker push lorkorblaq/labpal_gpt:latest
+# docker build -t lorkorblaq/labpal_gpt:latest -f ../../labpal_gpt/Dockerfile ../../labpal_gpt
+# docker push lorkorblaq/labpal_gpt:latest
