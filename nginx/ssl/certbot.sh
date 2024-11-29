92a8134319f123f76aad3b3decf40ce35ae6d769
lorkorblaq <f3miolok@gmail.com>
2024-11-29 20:30:11 +0100

    update

--- nginx/ssl/certbot.sh
+++ nginx/ssl/certbot.sh
@@ -2,22 +2,18 @@
 
 # Update system and install certbot
 sudo apt-get update
-sudo apt-get install -y certbot
+sudo apt-get install -y certbot python3-certbot-nginx
 
-# Make sure Docker volume directory exists
-WEBROOT_DIR="/var/lib/docker/volumes/letsencrypt_data/_data"
+# Make sure Nginx is running
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+# Define domain and email
 DOMAIN="labpal.com.ng"
 EMAIL="f3mioloko@gmail.com"
 
-# Run Certbot to obtain/renew certificate
-certbot certonly \
-  --webroot -w $WEBROOT_DIR \
-  -d $DOMAIN -d www.$DOMAIN \
-  --non-interactive \
-  --agree-tos \
-  --email $EMAIL \
-  --force-renewal \
-  >> /var/log/certbot-renew.log 2>&1
+# Run Certbot using the Nginx plugin to obtain/renew certificate
+sudo certbot --nginx -d $DOMAIN -d www.$DOMAIN --non-interactive --agree-tos --email $EMAIL --force-renewal >> /var/log/certbot-renew.log 2>&1
 
 # Check if Certbot was successful
 if [ $? -eq 0 ]; then
@@ -28,7 +24,7 @@ else
 fi
 
 # Add the cron job for automatic renewal (if not already added)
-CRON_JOB="0 3 * * * certbot renew --webroot -w $WEBROOT_DIR --quiet >> /var/log/certbot-renew.log 2>&1"
+CRON_JOB="0 3 * * * certbot renew --quiet >> /var/log/certbot-renew.log 2>&1"
 
 # Check if the cron job already exists
 (crontab -l 2>/dev/null | grep -F "$CRON_JOB") || (echo "$CRON_JOB" | crontab -)
